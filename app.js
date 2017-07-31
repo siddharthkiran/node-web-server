2a76dd9f27ea1e421e2af780108de855353851d8
siddharthshimpi <shimpi.siddharth91@gmail.com>
2017-07-31 13:36:55 -0500

    setip startstrip and jheroku port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app= express();
 
 hbs.registerPartials(__dirname + "/views/partials")
@@ -40,4 +41,6 @@ app.get('/bad',(req,res)=>{
   })
 })
 
-app.listen(3000);
+app.listen(port, () =>{
+  console.log(`server is on port ${port}`);
+});
